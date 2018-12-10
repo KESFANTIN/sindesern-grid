aff290e4aeea2ee773784447b22cb9efead5f51d
klaytonfantin <klayton.rammstein@gmail.com>
2018-12-10 17:17:36 -0200

    Java Script Menu

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+$(function(){
+    // Aparece e esconde menu
+    var navegation = document.getElementById('navegation');
+    var headroom = new Headroom(navegation);
+    headroom.init();
+
+
+    //Menu Responsive
+    // Calcula a largura da página
+    var largura = $(window).width(),
+        enlaces = $('enlaces'),
+        btnMenu = $('btn-menu'),
+        icone = $('btnMenu, icone');
+
+        if(largura < 768){
+            enlaces.hide();
+        }
+});
