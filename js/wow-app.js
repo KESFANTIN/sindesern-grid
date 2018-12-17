a142fa688acaf7cc2b2c0a2f1b6a5bbff6718451
klaytonfantin <klayton.rammstein@gmail.com>
2018-12-17 10:56:43 -0200

    WOW

---
+++ js/wow-app.js
@@ -0,0 +1,8 @@
+wow = new WOW({
+    boxClass: 'wow', // default
+    animateClass: 'animated', // default
+    offset: 20, // default
+    mobile: false, // default
+    live: true // default
+})
+wow.init();
